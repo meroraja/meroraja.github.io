fd2535bd2ceb6ab79943068ff1b27f79f702664b
Mero Raja <meroraja@roosterlogic.com>
2021-06-26 15:37:25 +0545

    Updated to show time in different timezones according to game time and selected time

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
+var choosenTime='0';
+var fixedTimeZone = 'Atlantic/South_Georgia';
+
 function displayTime() {
 
     var formatString='HH:mm:ss Z'
     var zones = JSON.parse(data);
+    var gameToday=moment().tz(fixedTimeZone).startOf('day').add(choosenTime, 'hours');
 
     zones.map(function(item) {
             item['time']=moment().tz(item['zone']).format(formatString);
+            item['future_time']=(gameToday.tz(item['zone']).clone().tz(item['zone'])).calendar();
             return item;
         });
 
@@ -12,8 +17,8 @@ function displayTime() {
     itemValue2+='<div class="blog-entry">'
     itemValue2+='<div class="text d-flex">'
     itemValue2+='<blockquote class="blockquote">'
-    itemValue2+='<h4 class="name"></h4>'
-    itemValue2+=' <em><small><span class="badge badge-primary"></span></small></em>'
+    itemValue2+='<span><h5 class="name"></h5>'
+    itemValue2+=' <em><span class="badge badge-secondary"><strong class="future_time"></strong></span></em><span>'
     itemValue2+='<footer class="blockquote-footer"><strong class="time"></strong> <span> - <small class="zone"></small></span></footer>'
     itemValue2+='</blockquote>'
     itemValue2+='</div>'
@@ -22,7 +27,7 @@ function displayTime() {
     itemValue2+='</div>'
 
     var options ={
-        valueNames: ['name','time','zone'],
+        valueNames: ['name','time','zone','future_time'],
         item: itemValue2,
         page: 30,
         pagination: [{
@@ -40,4 +45,32 @@ function displayTime() {
 
 $(document).ready(function() {
     displayTime();
+    instantiateTimePicker();
 });
+
+
+function instantiateTimePicker(){
+    var timeOptions=[{'value':'0','label':'Reset'}]
+
+    for (var i = 1; i < 24; i++) {
+        timeOptions.push({'value':i,'label':'+'+i})
+    }
+    populateSelect('expectedTime',timeOptions);
+
+     $('#expectedTime').on('change',function(){
+        var chosenTime = $('#expectedTime').val();
+        choosenTime=chosenTime
+     });
+}
+
+function populateSelect(id,data){
+    // IdName :- ID name of select option
+    // Data :- Dict with option value and label to populate
+    var html='';
+    html+='<option value="" selected>Choose...</option>'
+    data.forEach(function(option){
+        html+='<option value="'+option.value+'">'+option.label+'</option>';
+    });
+    $('#'+id).html(html);
+    $('#'+id).selectpicker('refresh');
+}
